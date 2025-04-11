c3db2181b0b71b038022f32803837ea67775b1db
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-04-11 17:22:27 +0200

    8 current   2025-04-11 17:22:25  25.05.20250409.f675531   6.12.22                          stable-mesa

--- nixos/modules/variables.nix
+++ nixos/modules/variables.nix
@@ -5,5 +5,11 @@
 }: {
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+    LD_LIBRARY_PATH = lib.makeLibraryPath [
+      pkgs.xorg.libXcursor
+      pkgs.xorg.libXrandr
+      pkgs.xorg.libXxf86vm
+      pkgs.libGL
+    ];
   };
 }
